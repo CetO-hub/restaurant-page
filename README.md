6d2817c51bd6418092fd03f2f19663e0143509e9
CetO-hub <skynet@SkyNet.localdomain>
2022-06-19 19:08:50 +0200

    Add "about mjami" text content

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Create a restaurant web page using javascript only in combination with webpack.
 Plan
 
 The web page should have 3 different and clickable tabs (main, menu and contact).
+
+Assets:
+
+Logo: https://de.freelogodesign.org
+Background-Image: https://pixabay.com/de/photos/messer-lebensmittel-holz-tisch-2879330/
+Owner: https://www.freepik.com/free-photo/mid-shot-chef-holding-plate-with-pasta-making-ok-sign_11628678.htm#query=cook&position=31&from_view=search
--- src/initial.js
+++ src/initial.js
@@ -1,5 +1,5 @@
 import Logo from "./logo.png";
-
+import Owner from "./owner.jpg";
 const displayContent = (() => {
   const about = () => {
     const isContent = document.createElement("div");
@@ -11,21 +11,51 @@ const displayContent = (() => {
     const isHeadlineListItem2 = document.createElement("li");
     const isHeadlineListItem3 = document.createElement("li");
     const isBodyContainer = document.createElement("div");
-    const isLogo = new Image();
+    const isAbout = document.createElement("div");
+    const isAboutText1 = document.createElement("p");
+    const isAboutText2 = document.createElement("p");
+    const isAboutFeatures1 = document.createElement("p");
+    const isAboutFeatures2 = document.createElement("p");
+    const isAboutFeatures3 = document.createElement("p");
+    const isAboutHead = document.createElement("h2");
 
+    const isLogo = new Image();
+    const isOwner = new Image();
+
+    isLogo.src = Logo;
+    isOwner.src = Owner;
     isContent.id = "content";
+
     isHeadlineContainer.classList.add("headline-container");
     isHeadlineImageContainer.classList.add("headline-image-container");
-    isLogo.src = Logo;
     isHeadlineList.classList.add("headline-list");
     isHeadlineListItem1.classList.add("headline-list-item");
     isHeadlineListItem1.classList.add("active");
     isHeadlineListItem2.classList.add("headline-list-item");
     isHeadlineListItem3.classList.add("headline-list-item");
     isBodyContainer.classList.add("body-container");
+    isAbout.classList.add("about-container");
+    isOwner.classList.add("about-owner");
+    isAboutText1.classList.add("about-text");
+    isAboutText2.classList.add("about-text");
+    isAboutFeatures1.classList.add("about-text");
+    isAboutFeatures2.classList.add("about-text");
+    isAboutFeatures3.classList.add("about-text");
+
     isHeadlineListItem1.textContent = "About mjami";
     isHeadlineListItem2.textContent = "Menu";
     isHeadlineListItem3.textContent = "Contact us";
+    isAboutHead.textContent = "Become a part of the #zerowaste movement";
+    isAboutText1.textContent =
+      "There's a new kid in town - and it's changing the way we think about food.";
+    isAboutText2.textContent =
+      "It's time to stop throwing food scraps away like they didn't matter and start eating biscuits from the ground!";
+    isAboutFeatures1.textContent =
+      "Your new favorite restaurant mjami is your go-to for those looking for a zero waste dining experience that offers both healthy and delicious options.";
+    isAboutFeatures2.textContent =
+      "With our 100% local kitchen, fair pricing, and spacious tables, we make it easy for you to dine with friends and family!  Eat healthy with us!";
+    isAboutFeatures3.textContent =
+      "Our kitchen is run using sustainable practices and we use only reusable packaging - so you can eat guilt-free without compromising on quality.";
 
     isHeadlineImageContainer.appendChild(isLogo);
     isHeadlineContainer.appendChild(isHeadlineImageContainer);
@@ -35,6 +65,14 @@ const displayContent = (() => {
     isHeadlineList.appendChild(isHeadlineListItem3);
     isContent.appendChild(isHeadlineContainer);
     isContent.appendChild(isBodyContainer);
+    isAbout.appendChild(isAboutHead);
+    isAbout.appendChild(isOwner);
+    isAbout.appendChild(isAboutText1);
+    isAbout.appendChild(isAboutText2);
+    isAbout.appendChild(isAboutFeatures1);
+    isAbout.appendChild(isAboutFeatures2);
+    isAbout.appendChild(isAboutFeatures3);
+    isBodyContainer.appendChild(isAbout);
     document.body.appendChild(isContent);
   };
   return { about };
